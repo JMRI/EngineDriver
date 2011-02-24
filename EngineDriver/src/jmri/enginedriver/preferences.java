5348d69dee507a576c8e1ac28afcf2149d77da3e
Steve Todd <mstevetodd@mstevetodd.com>
2011-02-24 03:05:13 +0000

    moved default throttle naming to connection.oncreate and preferences (like an edit)

--- EngineDriver/src/jmri/enginedriver/preferences.java
+++ EngineDriver/src/jmri/enginedriver/preferences.java
@@ -1,14 +1,58 @@
 package jmri.enginedriver;
 
-import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import java.util.Random;
 
-public class preferences extends PreferenceActivity {
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.provider.Settings;
+
+public class preferences extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 
 	  /** Called when the activity is first created. */
 	  @Override
 	  public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    addPreferencesFromResource(R.xml.preferences);
+	  }
+	  
+	  @Override
+	  protected void onResume() {
+	      super.onResume();
+
+	      // Set up a listener whenever a key changes            
+	      getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	  }
+
+	  @Override
+	  protected void onPause() {
+	      super.onPause();
+
+	      // Unregister the listener            
+	      getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);    
+	  }
+
+	  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		  if (key.equals("throttle_name_preference"))  {
+			  String defaultName = getApplicationContext().getResources().getString(R.string.prefThrottleNameDefaultValue);
+			  String currentValue = sharedPreferences.getString(key, defaultName).trim();
+			  //if new name is blank or the default name, make it unique
+			  if (currentValue.equals("") || currentValue.equals(defaultName)) {
+				  String deviceId = Settings.System.getString(getContentResolver(), Settings.System.ANDROID_ID);
+				  if (deviceId != null && deviceId.length() >=4) {
+					  deviceId = deviceId.substring(deviceId.length() - 4);
+				  } else {
+			        	Random rand = new Random();
+			        	deviceId = String.valueOf(rand.nextInt(9999));  //use random string
+				  }
+				  String uniqueDefaultName = defaultName + " " + deviceId;
+				  sharedPreferences.edit().putString(key, uniqueDefaultName).commit();  //save new name to prefs
+			  }
+
+
+		  }
+
 	  }
 }
