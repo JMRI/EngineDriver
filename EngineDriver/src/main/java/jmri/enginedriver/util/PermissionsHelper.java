bd06c3358e9424d7da1c23917231c8148663245a
flash62au <akersp62@gmail.com>
2020-07-21 20:56:58 +1000

    NEW - Pause and limit speed options for GamePads

--- EngineDriver/src/main/java/jmri/enginedriver/util/PermissionsHelper.java
+++ EngineDriver/src/main/java/jmri/enginedriver/util/PermissionsHelper.java
@@ -44,9 +44,7 @@ public class PermissionsHelper {
             ACCESS_COARSE_LOCATION,
             STORE_SERVER_AUTO_PREFERENCES,
             READ_SERVER_AUTO_PREFERENCES,
-            VIBRATE,
-            CHANGE_NETWORK_STATE_ON,
-            CHANGE_NETWORK_STATE_OFF
+            VIBRATE
     })
     public @interface RequestCodes {}
 
@@ -69,8 +67,6 @@ public class PermissionsHelper {
     public static final int STORE_LOG_FILES = 45;
     public static final int VIBRATE = 46;
     public static final int READ_IMAGES = 47;
-    public static final int CHANGE_NETWORK_STATE_ON = 48;
-    public static final int CHANGE_NETWORK_STATE_OFF = 49;
 
     private boolean isDialogOpen = false;
     private static PermissionsHelper instance = null;
@@ -165,9 +161,6 @@ public class PermissionsHelper {
                 return context.getResources().getString(R.string.permissionsACCESS_COARSE_LOCATION);
             case VIBRATE:
                 return context.getResources().getString(R.string.permissionsVIBRATE);
-           case CHANGE_NETWORK_STATE_ON:
-           case CHANGE_NETWORK_STATE_OFF:
-                return context.getResources().getString(R.string.permissionsCHANGE_NETWORK_STATE);
             default:
                 return "Unknown permission request: " + requestCode;
         }
@@ -238,13 +231,6 @@ public class PermissionsHelper {
                     Log.d("Engine_Driver", "Requesting VIBRATE permissions");
                     activity.requestPermissions(new String[]{
                                     Manifest.permission.VIBRATE},
-                            requestCode);
-                    break;
-                case CHANGE_NETWORK_STATE_ON:
-                case CHANGE_NETWORK_STATE_OFF:
-                    Log.d("Engine_Driver", "Requesting CHANGE_NETWORK_STATE permissions");
-                    activity.requestPermissions(new String[]{
-                                    Manifest.permission.CHANGE_NETWORK_STATE},
                             requestCode);
                     break;
             }
@@ -376,9 +362,6 @@ public class PermissionsHelper {
                 return result;
             case VIBRATE:
                 return ContextCompat.checkSelfPermission(context, Manifest.permission.VIBRATE) == PackageManager.PERMISSION_GRANTED;
-            case CHANGE_NETWORK_STATE_ON:
-            case CHANGE_NETWORK_STATE_OFF:
-                return ContextCompat.checkSelfPermission(context, Manifest.permission.CHANGE_NETWORK_STATE) == PackageManager.PERMISSION_GRANTED;
             default:
                 return false;
         }
@@ -418,9 +401,6 @@ public class PermissionsHelper {
                 return ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_COARSE_LOCATION);
             case WRITE_SETTINGS:
                 return ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.WRITE_SETTINGS);
-            case CHANGE_NETWORK_STATE_ON:
-            case CHANGE_NETWORK_STATE_OFF:
-                return ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.CHANGE_NETWORK_STATE);
             default:
                 return false;
         }
